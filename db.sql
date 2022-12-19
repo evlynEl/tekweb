e8a05c73eb573b5ddd457aa45061811084709e0b
Evelyn <evelyneleonora38@gmail.com>
2022-12-19 21:42:47 +0700

    database kategori & dokumen

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 21, 2022 at 11:27 AM
+-- Generation Time: Dec 19, 2022 at 03:42 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -24,19 +24,83 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `documents`
+--
+
+CREATE TABLE `documents` (
+  `id` int(11) NOT NULL,
+  `penulis` varchar(50) NOT NULL,
+  `judul` varchar(80) NOT NULL,
+  `kategori_id` int(2) NOT NULL,
+  `rating` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `documents`
+--
+
+INSERT INTO `documents` (`id`, `penulis`, `judul`, `kategori_id`, `rating`) VALUES
+(1, 'Alexander Chandra', 'Putih Abu - Abu', 10, 6),
+(2, 'J.K. Rowling', 'Hery Potah', 10, 9),
+(3, 'Andreas Gunawan', 'Menanam Hidroponik Di Gedung Bertingkat Dengan Pengaturan Ruang Segi Enam', 20, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `category_id` int(2) NOT NULL,
+  `category_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`category_id`, `category_name`) VALUES
+(10, 'Novel'),
+(20, 'Makalah'),
+(30, 'Skripsi');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `username` varchar(80) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(80) NOT NULL
+  `email` varchar(20) NOT NULL,
+  `password` varchar(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(1, 'Evelyn', 'evelyn@yahoo.com', '$2y$10$CGD5TlLtH3u6bfd04rDs5OGC11UjlQkJQyKYVnYBQLGDo0xAxYXlO'),
+(2, 'raditya_dika', 'radit@gmail.com', '$2y$10$HPGDT1AbEFylAJNC9KDgkuWxviSXyPL4wki9.QM7IXxsPEmW7IBeS'),
+(3, 'admin', 'admin@gmail.com', '$2y$10$PLtcwybV5LNzJe4drNWYfeCj5A/6fpNyLpeC0pqHtBRqBpaYo9MQ2');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `documents`
+--
+ALTER TABLE `documents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`category_id`);
 
 --
 -- Indexes for table `users`
@@ -50,10 +114,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `documents`
+--
+ALTER TABLE `documents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
