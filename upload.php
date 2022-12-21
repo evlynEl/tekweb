c9bae5c676ff8e9c7d6bd81b1a0c14f7dfbc2dda
Meagee69 <108181724+Meagee69@users.noreply.github.com>
2022-12-21 23:39:36 +0700

    music player

--- upload.php
+++ upload.php
@@ -103,7 +103,7 @@ if(isset($_POST['proses'])){
     <!-- Navbar -->
     <div class="container-fluid">
         <nav class="navbar navbar-dark navbar-expand-lg fixed-top">
-            <a class="navbar-brand">LOGO</a>
+        <a class="navbar-brand"><img class="logo" src="asset/img/logo.png"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
